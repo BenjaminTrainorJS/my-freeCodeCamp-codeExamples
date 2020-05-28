0a9e137f37e244c69b2e862658a3a536ca553735
Benjamin Trainor <benjamin.trainor.js@protonmail.com>
2020-05-28 11:22:51 +0930

    added new files with solutions to 6 Object-Orientated FCC challenges

---
+++ object-oriented-programming/create-a-method-on-an-object.js
@@ -0,0 +1,16 @@
+// Using the dog object, give it a method called sayLegs. 
+// The method should return the sentence "This dog has 4 legs."
+
+let dog = {
+    name: "Spot",
+    numLegs: 4,
+  
+  };
+  
+  dog.sayLegs = function() {
+    return "This dog has " + dog.numLegs + " legs.";
+  }
+  
+  dog.sayLegs()
+  
+  console.log(dog.sayLegs());
